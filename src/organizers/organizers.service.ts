d8e22fb18b6401ae8991ac543f85ed68bac3c035
saputt <sauki666indo@gmail.com>
2026-09-17 16:33:30 +0700

    feat(organizers): add validation to create organizers

--- src/organizers/organizers.service.ts
+++ src/organizers/organizers.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { OrganizersRepository } from './organizers.repository';
 import { CreateOrganizerDto } from './dto/create-organizer.dto';
 import { UpdateOrganizerDto } from './dto/update-organizer.dto';
@@ -16,8 +16,20 @@ export class OrganizersService {
 
         return organizer
     }
+    
+    async isUserAlreadyHaveOrganizer(userId: string) {
+        const organizer = await this.organizerRepo.findOrganizerByUserId(userId)
+
+        if (organizer) {
+            throw new ConflictException('You already have organizer')
+        }
+
+        return organizer
+    }
 
     async createOrganizer(dto: CreateOrganizerDto, userId: string) {
+        await this.isUserAlreadyHaveOrganizer(userId)
+
         return this.organizerRepo.createOrganizer(dto, userId)
     }
 
