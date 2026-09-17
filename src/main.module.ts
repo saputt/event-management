41e17be8114a1c320edf96c929b5b207692e6696
saputt <sauki666indo@gmail.com>
2026-09-17 15:13:02 +0700

    chore: setup prisma

--- src/main.module.ts
+++ src/main.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { EventsModule } from './events/events.module';
 import { OrganizersModule } from './organizers/organizers.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, EventsModule, OrganizersModule]
+  imports: [AuthModule, EventsModule, OrganizersModule, PrismaModule]
 })
 export class AppModule {}
